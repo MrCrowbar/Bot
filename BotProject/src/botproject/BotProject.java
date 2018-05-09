87925fa0845da54cc63f9373bc080688bb578687
MrCrowbar <crowbar@MrCrowbars-MacBook-Pro.local>
2018-05-09 15:38:21 -0500

    Añadí función de huesos con prayer, ya está testeado

--- BotProject/src/botproject/BotProject.java
+++ BotProject/src/botproject/BotProject.java
@@ -1,65 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package botproject;
+
 import java.awt.Robot;
 import java.awt.AWTException;
 import java.awt.event.MouseEvent;
 
-/**
- *
- * @author crowbar
- */
 public class BotProject {
-
     private static Robot myRobot = null;
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         try {
             myRobot = new Robot();
         }
         catch(AWTException e){
             e.printStackTrace();
         }
-        //for (int i = 0; i < 2; i++){
             klick();
-        //}
-    }
-    public static void klick(){
-        myRobot.delay(1000);
-        myRobot.mouseMove(75,100);
-        myRobot.mousePress(MouseEvent.BUTTON1_MASK);
-        myRobot.delay(1000);
-        myRobot.mouseRelease(MouseEvent.BUTTON1_MASK);
-        
-        myRobot.mouseMove(75,100);
-        myRobot.mousePress(MouseEvent.BUTTON1_MASK);
-        myRobot.delay(500);
-        myRobot.mouseRelease(MouseEvent.BUTTON1_MASK);
-        myRobot.delay(1000);
-        
-        myRobot.mouseMove(75,140);
-        myRobot.mousePress(MouseEvent.BUTTON1_MASK);
-        myRobot.delay(500);
-        myRobot.mouseRelease(MouseEvent.BUTTON1_MASK);
-        myRobot.delay(1000);
-        
-        myRobot.mouseMove(75,180);
-        myRobot.mousePress(MouseEvent.BUTTON1_MASK);
-        myRobot.delay(500);
-        myRobot.mouseRelease(MouseEvent.BUTTON1_MASK);
-        myRobot.delay(1000);
-        
-        myRobot.mouseMove(75,220);
-        myRobot.mousePress(MouseEvent.BUTTON1_MASK);
-        myRobot.delay(500);
-        myRobot.mouseRelease(MouseEvent.BUTTON1_MASK);
-        myRobot.delay(1000);
     }
     
+    public static void klick(){
+        int cantY = 100;
+        int cantX = 75;
+        //Dar click para activar ventana
+        myRobot.delay(1000);
+        myRobot.mouseMove(75,100);
+        myRobot.mousePress(MouseEvent.BUTTON1_MASK);
+        myRobot.delay(1000);
+        myRobot.mouseRelease(MouseEvent.BUTTON1_MASK);
+        
+        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 5; j++){
+                myRobot.mouseMove(cantX,cantY);
+                myRobot.mousePress(MouseEvent.BUTTON1_MASK);
+                myRobot.delay(500);
+                myRobot.mouseRelease(MouseEvent.BUTTON1_MASK);
+                myRobot.delay(1000);
+                cantX+=40;
+            }
+            cantX = 75;
+            cantY+= 40;
+        }
+    }
 }
