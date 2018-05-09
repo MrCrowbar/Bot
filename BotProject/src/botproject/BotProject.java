54a45b1a3ac39dcc9cc47730538bb69fe95da7c1
MrCrowbar <crowbar@MrCrowbars-MacBook-Pro.local>
2018-05-09 06:20:51 -0500

    Copié el antiguo archivo de Robot

--- BotProject/src/botproject/BotProject.java
+++ BotProject/src/botproject/BotProject.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package botproject;
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.event.MouseEvent;
 
 /**
  *
@@ -11,11 +14,27 @@ package botproject;
  */
 public class BotProject {
 
+    private static Robot myRobot = null;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HEllo woorld");
+        // TODO code application logic here
+        try {
+            myRobot = new Robot();
+        }
+        catch(AWTException e){
+            e.printStackTrace();
+        }
+        klick();
+    }
+    public static void klick(){
+        myRobot.delay(1000);
+        myRobot.mouseMove(500,500);
+        myRobot.mousePress(MouseEvent.BUTTON1_MASK);
+        myRobot.mouseRelease(MouseEvent.BUTTON1_MASK);
+        myRobot.delay(1000);
+        myRobot.mouseMove(750,750);
     }
     
 }
